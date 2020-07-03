af410804838c7875d59ebe2f82d7bafcfb5194da
Gina Frankel <g.frankel22@gmail.com>
2020-07-03 14:32:28 +0100

    Delete boilerplate + add CV link

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Gina-Frankel/CV"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github CV
         </a>
       </header>
     </div>
