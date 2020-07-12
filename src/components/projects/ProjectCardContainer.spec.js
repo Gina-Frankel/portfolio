753507ba42e27c12ab4cb4b9f64b634a6dd9d45a
Guy Katsav <guykatsav@MacBook-Pro-2.local>
2020-07-12 21:14:53 +0100

    Change name to ProjectCardContainer

--- src/components/projects/ProjectCardContainer.spec.js
+++ src/components/projects/ProjectCardContainer.spec.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {shallow} from "enzyme"
 import { render } from '@testing-library/react';
-import Projects from "./Projects"
+import Projects from "./ProjectCardContainer"
 
 it("renders correctly", () => {
 
