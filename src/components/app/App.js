ff2bd5c4aab3d4e8c61cb6d8346b9586de925fd1
Guy Katsav <guykatsav@MacBook-Pro-2.local>
2020-07-12 17:17:54 +0100

    Delete comments

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -45,33 +45,4 @@ function App() {
 }
 
 
-{/* <Router>
-<div>
-  <nav>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/about">About</Link>
-      </li>
-      <li>
-        <Link to="/users">Users</Link>
-      </li>
-    </ul>
-  </nav> */}
-//   <Switch>
-//   <Route path="/about">
-//     <About />
-//   </Route>
-//   <Route path="/users">
-//     <Users />
-//   </Route>
-//   <Route path="/">
-//     <Home />
-//   </Route>
-// </Switch>
-// </div>
-// </Router>
-// );
 export default App;
