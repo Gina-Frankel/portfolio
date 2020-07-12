b7dbc95a986c977fc2eafde2a4df58d84c837ecd
Guy Katsav <guykatsav@MacBook-Pro-2.local>
2020-07-12 15:33:01 +0100

    Move files inside component/app

---
+++ src/components/app/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/Gina-Frankel/CV"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github CV
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app/App.spec.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {shallow} from "enzyme"
+import App from "./App"
+
+
+
+it("renders without crashing", () => {
+ 
+  const wrapper = shallow(<App />);
+
+  expect(wrapper).toHaveLength(1)
+
+});
