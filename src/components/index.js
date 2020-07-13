0891519d1b89465e881b565090a316eb1334fbe1
Gina Frankel <g.frankel22@gmail.com>
2020-07-13 18:24:33 +0100

    Add InterestsContainerComponent to  export

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
+export { InterestsContainer } from "./interests/InterestsContainer";
