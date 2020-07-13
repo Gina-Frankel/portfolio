b6eb297409e1ae99a23459b23a183f43f1809283
Gina Frankel <g.frankel22@gmail.com>
2020-07-13 13:40:16 +0100

    Add technologies property

--- src/projects.js
+++ src/projects.js
@@ -2,14 +2,18 @@ const projectList = [
   {
     name: "Mission Ctrl",
     repoLink: "https://github.com/tommyrharper/mission-ctrl",
+    technologies: "MongoDB, Express, React, Node, SCSS",
   },
   {
     name: "Bank Tech Test",
     repoLink: "https://github.com/Gina-Frankel/bank_tech_tests",
+    technologies: "Ruby",
   },
   {
     name: "Statues",
     repoLink: "https://github.com/Gina-Frankel/statues",
+    technologies:
+      "React, React Router, Javascript, Python, Spacey, Beautiful Soup",
   },
 ];
 
