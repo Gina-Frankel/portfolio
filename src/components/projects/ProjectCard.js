0af9f84bc12143612d860e2787ced6bef467856c
Gina Frankel <g.frankel22@gmail.com>
2020-07-13 11:57:26 +0100

    Add method to iterate over ProjectList  + render each project

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,10 +1,16 @@
 import React from "react";
+import projectList from "../../projects";
 
 function ProjectCard() {
+  //console.dir(projectList);
+  const projects = projectList.map((projectItem, index) => (
+    //console.dir(projectItem.name)
+    <li key={index}>{projectItem.name}</li>
+  ));
+  //console.dir(projects);
   return (
     <div>
-      <h3>Mission Ctrl</h3>
-
+      <ul>{projects}</ul>
       <a
         className="Project-Link"
         href="https://github.com/tommyrharper/mission-ctrl"
