a34c06064a7b4ffdb8a0554c1fada50168f9c4fa
Gina Frankel <g.frankel22@gmail.com>
2020-07-13 11:55:48 +0100

    Add repoLinks

--- src/projects.js
+++ src/projects.js
@@ -1,11 +1,16 @@
-const projectsList = [
+const projectList = [
   {
     name: "Mission Ctrl",
+    repoLink: "https://github.com/tommyrharper/mission-ctrl",
   },
   {
     name: "Bank Tech Test",
+    repoLink: "https://github.com/Gina-Frankel/bank_tech_tests",
   },
   {
     name: "Statues",
+    repoLink: "https://github.com/Gina-Frankel/statues",
   },
 ];
+
+export default projectList;
