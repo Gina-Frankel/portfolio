e3b92eda28e4c633f0cd48630c64ecc617311c30
Gina Frankel <g.frankel22@gmail.com>
2020-07-14 10:51:02 +0100

    Add export of LinkContainer component

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
 export { InterestsContainer } from "./interests/InterestsContainer";
+export { LinkContainer } from "./links/LinkContainer";
