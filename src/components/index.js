44e6a19375ffed0217b5ea1eaea54ee7ed893cd7
Gina Frankel <g.frankel22@gmail.com>
2020-07-16 21:37:59 +0100

    Delete export of InterestsContainer

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
-export { InterestsContainer } from "./interests/InterestsContainer";
 export { LinkContainer } from "./links/LinkContainer";
