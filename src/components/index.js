1f7ecb2d501994bea90117469b3c0bff2a1a5e76
Gina Frankel <g.frankel22@gmail.com>
2020-07-16 21:27:17 +0100

    Import bootstrap

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
