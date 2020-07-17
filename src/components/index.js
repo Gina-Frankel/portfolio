db0567ab91a3f0ff5bd1f31896769b74437e1b13
Gina Frankel <g.frankel22@gmail.com>
2020-07-17 11:03:25 +0100

    Export ProjectCardContainer

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
+export { ProjectCardItem } from "./projects/ProjectCardItem";
 export { LinkContainer } from "./links/LinkContainer";
