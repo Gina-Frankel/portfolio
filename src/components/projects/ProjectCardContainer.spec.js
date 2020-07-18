444439fe3c27dfba274c58196ea3bc385952a650
Gina Frankel <g.frankel22@gmail.com>
2020-07-18 16:37:46 +0100

    Delete unneeded comments + import

--- src/components/projects/ProjectCardContainer.spec.js
+++ src/components/projects/ProjectCardContainer.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { render } from "@testing-library/react";
+
 import { ProjectCardContainer } from "../index";
 import { ProjectCard } from "../index";
 
@@ -16,22 +16,9 @@ it("renders Projects", () => {
   expect(wrapper.text()).toContain("Projects");
 });
 
-// it("renders a Link to the repoLink", () => {
-//     const wrapper = shallow(<Projects  />)
-//     const link = (
-//       <a href="https://github.com/Gina-Frankel/CV">
-//         GitHub Repo
-//       </a>
-//     )
-//     console.dir(wrapper)
-//     expect(wrapper.text()).toContain(link)
-//     })
-
 it("renders a ProjectCard", () => {
   const wrapper = shallow(<ProjectCardContainer />);
   const projectCard = <ProjectCard />;
 
   expect(wrapper.containsMatchingElement(projectCard)).toEqual(true);
 });
-
-//expect(wrapper.containsMatchingElement(statueMenuItem)).toEqual(true);
