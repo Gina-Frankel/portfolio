08b338e0b844e9bd35585bbc5adcdcc340819f17
Gina Frankel <g.frankel22@gmail.com>
2020-07-18 22:57:48 +0100

    Add _blank to target so links opens up on new page

--- src/components/links/LinkContainer.js
+++ src/components/links/LinkContainer.js
@@ -3,11 +3,35 @@ import React from "react";
 export function LinkContainer() {
   return (
     <div className="link">
-      <a href="https://github.com/Gina-Frankel">GitHub</a>
-      <br></br>
-      <a href="https://www.linkedin.com/in/gina-frankel">LinkedIn</a>
-      <br></br>
-      <a href="https://github.com/Gina-Frankel/CV">Github CV</a>
+      <p>
+        <a
+          href="https://github.com/Gina-Frankel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </p>
+
+      <p>
+        <a
+          href="https://www.linkedin.com/in/gina-frankel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
+
+      <p>
+        <a
+          href="https://github.com/Gina-Frankel/CV"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github CV
+        </a>
+      </p>
     </div>
   );
 }
