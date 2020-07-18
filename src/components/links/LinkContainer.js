f7a468ec5e57e17671369beabe0ad263afda6f65
Gina Frankel <g.frankel22@gmail.com>
2020-07-19 00:01:04 +0100

    Add bootstrap row with offset colums

--- src/components/links/LinkContainer.js
+++ src/components/links/LinkContainer.js
@@ -3,35 +3,37 @@ import React from "react";
 export function LinkContainer() {
   return (
     <div className="link">
-      <p>
-        <a
-          href="https://github.com/Gina-Frankel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </p>
+      <div className="row ">
+        <div className="">
+          <a
+            href="https://github.com/Gina-Frankel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
 
-      <p>
-        <a
-          href="https://www.linkedin.com/in/gina-frankel"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-      </p>
+        <div className="offset-md-1">
+          <a
+            href="https://www.linkedin.com/in/gina-frankel"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            LinkedIn
+          </a>
+        </div>
 
-      <p>
-        <a
-          href="https://github.com/Gina-Frankel/CV"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github CV
-        </a>
-      </p>
+        <div className="offset-md-1">
+          <a
+            href="https://github.com/Gina-Frankel/CV"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Github CV
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
