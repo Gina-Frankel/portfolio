3add2bbcd7165711fd85d4801071ab99d3dd6cc9
Gina Frankel <g.frankel22@gmail.com>
2020-07-19 00:58:18 +0100

    Add classNames

--- src/components/links/LinkContainer.js
+++ src/components/links/LinkContainer.js
@@ -1,38 +1,40 @@
 import React from "react";
+import "./LinkContainer.css";
 
 export function LinkContainer() {
   return (
-    <div className="link">
-      <div className="row ">
-        <div className="">
-          <a
-            href="https://github.com/Gina-Frankel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </div>
+    <div className="row ">
+      <div className="">
+        <a
+          className="contactLink"
+          href="https://github.com/Gina-Frankel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+      </div>
 
-        <div className="offset-md-1">
-          <a
-            href="https://www.linkedin.com/in/gina-frankel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-        </div>
+      <div className="offset-md-1">
+        <a
+          className="contactLink"
+          href="https://www.linkedin.com/in/gina-frankel"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
 
-        <div className="offset-md-1">
-          <a
-            href="https://github.com/Gina-Frankel/CV"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github CV
-          </a>
-        </div>
+      <div className="offset-md-1">
+        <a
+          className="contactLink"
+          href="https://github.com/Gina-Frankel/CV"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github CV
+        </a>
       </div>
     </div>
   );
