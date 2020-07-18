910632f79d7ae369288e96d6310345620e96fb5d
Gina Frankel <g.frankel22@gmail.com>
2020-07-18 11:28:03 +0100

    Extract  child component from return of map function

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,21 +1,22 @@
 import React from "react";
 import projectList from "../../projects";
+import { ProjectCardItem } from "../index";
 
 export function ProjectCard() {
-  //console.dir(projectList);
-  const projects = projectList.map((projectItem, index) => (
-    //console.dir(projectItem.name)
-
-    <div key={index}>
-      <h3> {projectItem.name}</h3>
-      <a className="Project-Link" href={projectItem.repoLink}>
-        GitHub Repo
-      </a>
-      <p>{projectItem.summary}</p>
-      <p className="font-weight-bold">{projectItem.technologies}</p>
-      <br></br>
-    </div>
+  const projectItems = projectList.map((projectItem, index) => (
+    <ProjectCardItem key={index} value={projectItem} />
   ));
-  //console.dir(projects);
-  return <ul>{projects}</ul>;
+
+  return <ul>{projectItems}</ul>;
 }
+
+// <div key={index}>
+//   <h3> {projectItem.name}</h3>
+//   <a className="Project-Link" href={projectItem.repoLink}>
+//     GitHub Repo
+//   </a>
+//   <p>{projectItem.summary}</p>
+//   <p className="font-weight-bold">{projectItem.technologies}</p>
+//   <br></br>
+// </div>
+//console.dir(projects);
