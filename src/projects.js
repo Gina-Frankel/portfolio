de3f03aa3b81a840e662dc0728822b84197f1804
Gina Frankel <g.frankel22@gmail.com>
2020-07-18 13:58:33 +0100

    Add image property + import MissionCtrlImage

--- src/projects.js
+++ src/projects.js
@@ -1,15 +1,19 @@
+import MissionCtrlImage from "./static/projectImages/mission-ctrl.png";
+
 const projectList = [
   {
     name: "Mission Ctrl",
     repoLink: "https://github.com/tommyrharper/mission-ctrl",
     technologies: "MongoDB, Express, React, Node, SCSS",
     summary: "An arcade style game to teach VSCode shortcuts",
+    image: MissionCtrlImage,
   },
   {
     name: "Bank Tech Test",
     repoLink: "https://github.com/Gina-Frankel/bank_tech_tests",
     technologies: "Ruby",
     summary: "A Command Line Interface program imitating a bank account",
+    image: "",
   },
   {
     name: "Statues",
@@ -18,6 +22,7 @@ const projectList = [
       "React, React Router, Javascript, Python, Spacey, Beautiful Soup",
     summary:
       "Currently developing website to provide historical context about public art so that people can have a deeper understanding of British involvement in the Slave Trade",
+    image: "",
   },
 ];
 
