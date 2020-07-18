0d717dff5fe883bcc59091f6993102d8a63f1ca6
Gina Frankel <g.frankel22@gmail.com>
2020-07-18 14:06:49 +0100

    Style image with bootstrap class so resizes

--- src/components/projects/ProjectCardItem.js
+++ src/components/projects/ProjectCardItem.js
@@ -2,12 +2,16 @@ import React from "react";
 
 export function ProjectCardItem(props) {
   return (
-    <div class="col-sm-12 col-lg-6 py-5">
+    <div className="col-sm-12 col-lg-6 py-5">
       <h3>{props.value.name}</h3>
       <p>{props.value.technologies}</p>
       <p>{props.value.summary}</p>
       <a href={props.value.repoLink}>GitHub Repo</a>
-      {/* <img src={props.value.image} alt="project image"></img> */}
+      <img
+        className="img-fluid"
+        src={props.value.image}
+        alt="project image"
+      ></img>
     </div>
   );
 }
