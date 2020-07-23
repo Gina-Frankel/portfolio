1ba5a993e11c3da2f99a688724795080b1c1c01c
Gina Frankel <g.frankel22@gmail.com>
2020-07-23 14:51:59 +0100

    Export ProjectLink component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ export { App } from "./app/App.js";
 export { ProjectCardContainer } from "./projects/ProjectCardContainer";
 export { ProjectCard } from "./projects/ProjectCard";
 export { ProjectCardItem } from "./projects/ProjectCardItem";
+export { ProjectLink } from "./projects/ProjectLink";
 export { LinkContainer } from "./links/LinkContainer";
 export { About } from "./about/About";
