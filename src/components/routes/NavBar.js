00303151ba561ed2e0ab1b6dfc61b9b90901cf13
Gina Frankel <g.frankel22@gmail.com>
2020-07-24 10:26:13 +0100

    Add AboutLink + ProjectLink component + style

--- src/components/routes/NavBar.js
+++ src/components/routes/NavBar.js
@@ -1,3 +1,17 @@
 import React from "react";
+import { AboutLink, ProjectLink } from "../index";
 
-export function NavBar() {}
+export function NavBar() {
+  return (
+    <div className="container">
+      <div className="d-flex flex-row bd-highlight justify-content-end">
+        <div className="p-2 bd-highlight">
+          <AboutLink />
+        </div>
+        <div className="p-2 bd-highlight">
+          <ProjectLink />
+        </div>
+      </div>
+    </div>
+  );
+}
