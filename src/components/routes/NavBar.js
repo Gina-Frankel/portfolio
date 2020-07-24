76353b7975a8ae04ad8783ad2b230b90ae9339f8
Gina Frankel <g.frankel22@gmail.com>
2020-07-24 11:42:33 +0100

    Change from div flex to nav elements

--- src/components/routes/NavBar.js
+++ src/components/routes/NavBar.js
@@ -1,17 +1,35 @@
 import React from "react";
 import { AboutLink, ProjectLink } from "../index";
+import { Link } from "react-router-dom";
 
 export function NavBar() {
   return (
-    <div className="container">
-      <div className="d-flex flex-row bd-highlight justify-content-end">
-        <div className="p-2 bd-highlight">
-          <AboutLink />
-        </div>
-        <div className="p-2 bd-highlight">
-          <ProjectLink />
-        </div>
-      </div>
+    <div className="container ">
+      <nav className="navbar sticky navbar-expand  ">
+        <ul className=" navbar-nav ml-auto ">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <p>About</p>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/Projects" className="nav-link">
+              <p>Projects</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
+
+// <div className="container">
+//
+//     <div className="p-2 bd-highlight">
+//       <AboutLink />
+//     </div>
+//     <div className="p-2 bd-highlight">
+//       <ProjectLink />
+//     </div>
+//   </div>
+// </div>
