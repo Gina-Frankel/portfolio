7bf14ef7c23b460d004da8fb4a10a62df951896c
Gina Frankel <g.frankel22@gmail.com>
2020-07-24 12:12:11 +0100

    Style ProjectCard add background for color + change column layout

--- src/components/projects/ProjectCardItem.js
+++ src/components/projects/ProjectCardItem.js
@@ -1,19 +1,27 @@
 import React from "react";
+import "./ProjectCardItem.css";
 
 export function ProjectCardItem(props) {
   return (
-    <div className="col-sm-12 col-lg-6 py-5">
-      <h3>{props.value.name}</h3>
-      <p>{props.value.technologies}</p>
-      <p>{props.value.summary}</p>
-      <a href={props.value.repoLink} target="_blank" rel="noopener noreferrer">
-        GitHub Repo
-      </a>
+    <div className="col-sm-12 col-lg-4 py-5  ">
       <img
         className="img-fluid"
         src={props.value.image}
         alt="project screenshot"
       ></img>
+
+      <div className="card-background px-3 pb-5">
+        <h3 className="pt-5 ">{props.value.name}</h3>
+        <p>{props.value.technologies}</p>
+        <p>{props.value.summary}</p>
+        <a
+          href={props.value.repoLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Repo
+        </a>
+      </div>
     </div>
   );
 }
