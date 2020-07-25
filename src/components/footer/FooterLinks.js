0f5e9e907e0a9bf3eb6cc769e4f4730b0e1e5fc4
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 13:57:34 +0100

    Return a link by accessing prop link + name value

--- src/components/footer/FooterLinks.js
+++ src/components/footer/FooterLinks.js
@@ -1,3 +1,15 @@
 import React from "react";
+// import "./Footer.css";
 
-export function FooterLinks() {}
+export function FooterLinks(props) {
+  return (
+    <a
+      className="contactLink  px-2"
+      href={props.value.link}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {props.value.name}
+    </a>
+  );
+}
