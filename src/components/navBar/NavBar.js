11a945644024f6d496b3bcd2263689818c0bb5e6
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 16:58:22 +0100

    Exracted NavLinks from NavBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,23 +1,11 @@
 import React from "react";
-
-import { Link } from "react-router-dom";
+import { NavLinks } from "../index";
 
 export function NavBar() {
   return (
     <nav className="navbar  sticky-top navbar-expand bg-light  ">
       <div className="container">
-        <ul className=" navbar-nav mr-auto ">
-          <li className="nav-item">
-            <Link to="/" className="nav-link">
-              <p>About</p>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/Projects" className="nav-link">
-              <p>Projects</p>
-            </Link>
-          </li>
-        </ul>
+        <NavLinks />
       </div>
     </nav>
   );
---
+++ src/components/navBar/NavLinks.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+export function NavLinks() {
+  return (
+    <ul className=" navbar-nav mr-auto ">
+      <li className="nav-item">
+        <Link to="/" className="nav-link">
+          <p>About</p>
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/Projects" className="nav-link">
+          <p>Projects</p>
+        </Link>
+      </li>
+    </ul>
+  );
+}
