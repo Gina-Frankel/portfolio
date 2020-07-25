be51a89c50f327d555b93cfbe9e5b100d596f0a0
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 17:07:32 +0100

    Move to navBar folder

--- src/components/navBar/ContactLinkContainer.js
+++ src/components/navBar/ContactLinkContainer.js
@@ -9,43 +9,8 @@ export function Footer() {
   ));
 
   return (
-    <footer id="stickyFooter" className="bg-dark ">
-      <div className="container d-flex justify-content-end">
-        <div>{FooterLinkItems}</div>
-      </div>
-    </footer>
+    <div className="container d-flex justify-content-end">
+      <div>{FooterLinkItems}</div>
+    </div>
   );
-}
-
-{
-  /* <a
-            className="contactLink"
-            href="https://github.com/Gina-Frankel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            GitHub
-          </a>
-        </div>
-
-        <div className="offset-md-1">
-          <a
-            className="contactLink"
-            href="https://www.linkedin.com/in/gina-frankel"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            LinkedIn
-          </a>
-        </div>
-
-        <div className="offset-md-1">
-          <a
-            className="contactLink"
-            href="https://github.com/Gina-Frankel/CV"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github CV
-          </a> */
 }
--- src/components/navBar/ProjectLink.spec.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { shallow } from "enzyme";
-import { ProjectLink } from "../index";
-
-it("renders correctly", () => {
-  const wrapper = shallow(<ProjectLink />);
-  expect(wrapper).toHaveLength(1);
-});
