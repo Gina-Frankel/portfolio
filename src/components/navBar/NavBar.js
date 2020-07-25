4a80176ad8b34987f1c3a2de117128293a0644a4
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 18:43:54 +0100

    Style navNar with padding + change color

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,8 +3,8 @@ import { ContactLinkList, NavPageLinks } from "../index";
 
 export function NavBar() {
   return (
-    <nav className="navbar  sticky-top navbar-expand bg-light  ">
-      <div className="container">
+    <nav className="navbar  sticky-top navbar-expand bg-dark  ">
+      <div className="container py-4">
         <NavPageLinks />
         <ContactLinkList />
       </div>
