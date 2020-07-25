eddeef02db9fb2b6b942ba7e8a85f1564c0ef8a6
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 16:37:43 +0100

    Fix test by adding mock prop

--- src/components/footer/FooterLinks.spec.js
+++ src/components/footer/FooterLinks.spec.js
@@ -2,7 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 import { FooterLinks } from "../index";
 
+const props = {
+  name: "Test contact",
+  link: "www.test.com",
+};
+
 it("renders correctly", () => {
-  const wrapper = shallow(<FooterLinks />);
+  const wrapper = shallow(<FooterLinks value={props} />);
   expect(wrapper).toHaveLength(1);
 });
