c60b78211319dc06b71d1a898636f9e26e36da3a
Gina Frankel <g.frankel22@gmail.com>
2020-07-25 13:54:13 +0100

    Create contactLinks object

---
+++ src/contactLinks.js
@@ -0,0 +1,13 @@
+const contactLinksList = [
+  {
+    name: "Github",
+    link: "https://github.com/Gina-Frankel",
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/gina-frankel",
+  },
+  { name: "Github CV", link: "https://github.com/Gina-Frankel/CV" },
+];
+
+export default contactLinksList;
