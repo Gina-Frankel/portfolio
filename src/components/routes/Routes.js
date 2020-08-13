07ab60c4bdd800feacaeb7c25202ebac35b5b18e
Gina Frankel <g.frankel22@gmail.com>
2020-08-13 20:04:31 +0100

    Add Route for ProjectProcessContainer

--- src/components/routes/Routes.js
+++ src/components/routes/Routes.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { About, NavBar, ProjectCardContainer } from "../index";
+import {
+  About,
+  NavBar,
+  ProjectCardContainer,
+  ProjectProcessContainer,
+} from "../index";
 export function Routes() {
   return (
     <Router>
       <NavBar />
 
       <Switch>
-        <Route path="/Projects">
-          <ProjectCardContainer />
-        </Route>
-        <Route path="/">
-          <About />
-        </Route>
+        <Route path="/Projects" component={ProjectCardContainer} />
+        <Route path="/test" component={ProjectProcessContainer} />
+
+        <Route path="/" component={About} />
       </Switch>
     </Router>
   );
