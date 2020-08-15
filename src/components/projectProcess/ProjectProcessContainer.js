878c985610d490792cc3fb26329a4e36e989a8e8
Gina Frankel <g.frankel22@gmail.com>
2020-08-15 12:09:29 +0100

    Use props to access project name + image

--- src/components/projectProcess/ProjectProcessContainer.js
+++ src/components/projectProcess/ProjectProcessContainer.js
@@ -1,33 +1,47 @@
 import React from "react";
-import projectList from "../../projects";
+// import projectList from "../../projects";
 import background from "../../static/linen.jpg";
+import "./ProjectProcessContainer.css";
 
-export function ProjectProcessContainer() {
-  console.log(projectList[0].image);
+export function ProjectProcessContainer(props) {
   return (
-    <div className="container">
-      <div className="jumpotron">
-        <h1 className="display-1  pt-5 "> Mission control</h1>
-        <hr class="my-4"></hr>
-      </div>
-
-      <div className="row">
-        <div class="col">
+    <div className="container ">
+      <div className="row pt-5">
+        <div className="col-lg-12 "></div>
+        <p className="col-lg-5">
+          <div className="jumpotron">
+            <h1 className="display-4  "> {props.project.name}</h1>
+          </div>
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum."
+          aliquip ex ea commodo consequat. Duis aute irure dolor in "<p></p>
+          <p>repo</p>
+          <p>See it live</p>
+        </p>
+
+        <div className=" col-sm-12  col-lg-7 pb-5  ">
+          <img
+            className="img-fluid"
+            src={props.project.image}
+            alt="linen"
+          ></img>
         </div>
-        <div className="col">
+
+        <div className="offset-lg-5 col-lg-7 ">
+          <h3> Design process </h3>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+
           <img className="img-fluid" src={background} alt="linen"></img>
         </div>
-      </div>
-
-      <div className="row">
-        <img className="img-fluid" src={background} alt="linen"></img>
       </div>
     </div>
   );
