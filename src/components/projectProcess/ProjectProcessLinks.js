b5b0090becf63f7a56eea2b39a39cd1295243e1f
Gina Frankel <g.frankel22@gmail.com>
2020-08-17 09:57:29 +0100

    Add span element + refactor githubLink to constant var

--- src/components/projectProcess/ProjectProcessLinks.js
+++ src/components/projectProcess/ProjectProcessLinks.js
@@ -4,25 +4,33 @@ export function ProjectProcessLinks(props) {
   let deployedLink;
   if (props.project.deployedLink) {
     deployedLink = (
-      <a
-        href={props.project.deployedLink}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <p>Visit Site</p>
-      </a>
+      <span className="px-3">
+        <a
+          href={props.project.deployedLink}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Site
+        </a>
+      </span>
     );
   }
-  return (
-    <div>
-      {" "}
+
+  const gitHubLink = (
+    <span>
       <a
         href={props.project.repoLink}
         target="_blank"
         rel="noopener noreferrer"
       >
-        <p> GitHub Repo </p>
+        GitHub Repo
       </a>
+    </span>
+  );
+  return (
+    <div>
+      <br></br>
+      {gitHubLink}
       {deployedLink}
     </div>
   );
