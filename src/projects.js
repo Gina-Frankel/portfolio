ff79b3b603a308a11a96351fe12fec2c18d764e5
Gina Frankel <g.frankel22@gmail.com>
2020-08-17 09:20:55 +0100

    Fix grammar

--- src/projects.js
+++ src/projects.js
@@ -17,7 +17,7 @@ const projectList = [
       introduction:
         "Mission Ctrl was my final group project while studying at Makers Academy. Mission Ctrl was born out of a need to learn shortcuts to help make writing code quicker. Visual Studio Code is a free source-code editor that many developers use. While studying at Makers Academy I was trying to learn the shortcuts but it was difficult to learn them while also trying to learn how to code.",
       explanation:
-        "The aesthetics for the project are based on 80s arcade style. We used a dark pixellated space themed background alongside pixellated text so as to create a lo-fi look. Flashes of bright colour referencing classic games such as space invaders, appear when you score a point.",
+        "The aesthetics for the project are based on 80s arcade style. We used a dark pixellated space themed background alongside pixellated text so as to create a lo-fi look. Flashes of bright colour referencing classic games such as space invaders, appear when you score a point. See image below for our inspiration board.",
       image: MissionCtrlInspirationImage,
     },
   },
@@ -56,6 +56,8 @@ const projectList = [
     process: {
       introduction:
         "Denmark Street Studios, a small business, asked me to find a way to help organise the spreadsheets of their bank account statements. They wanted a way to organise their bank statement by the company from where the transaction came from, as this would help them track and report their spending.",
+      explanation:
+        "Many spreadsheet programs such as numbers and  excel offer a way to organise your data. The way the sorting mechanism works is by looking through a column, which is chosen by the user, and then grouping the data in this column which is the same.For example if you had a column called  ‘pets’ with five animals, three of which were dogs and three of which were cats, then it would group all the cats together, and group all the dogs together. Generally in bank statement there is a column called ‘description’ which provides the company were the transaction came from but this is combined with other information, so one is not able to sort it from the description.\n\n I created a script with Python, that uses regex to look through the description to find the company name. The company name is then used to create a ‘company’ column of data. This ‘company’ column can be used to organise the statement by company names. The above image illustrates the result of running the script, opening up the updated spreadsheet in the program Numbers and then using Numbers category function to create a spreadsheet organised by companies.",
     },
   },
 
