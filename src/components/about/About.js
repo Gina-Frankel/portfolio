cffefc956a45745886ab340ce6f8434e7ee051ec
Gina Frankel <g.frankel22@gmail.com>
2020-09-10 20:17:50 +0100

    :lipstick: Stretch container across viewport + fix lead to bottom in About

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -3,10 +3,10 @@ import "./About.css";
 
 export function About() {
   return (
-    <div className="px-5">
+    <div className="px-5 container-fluid vertical-stretch">
       <div className="row justify-content-end ">
         <div className="col-9">
-          <h1 id="developerName" className="text-right">
+          <h1 id="developerName" className="text-right py-3">
             Gina Frankel
           </h1>
         </div>
@@ -18,12 +18,13 @@ export function About() {
           </h4>
         </div>
       </div>
-      <div className="row  py-5"></div>
-      <div className="row  py-5"></div>
-      <div className="row  py-5"></div>
-      <h4 id="lead">Design </h4>
-      <h4 id="lead"> Code </h4>
-      <h4 id="lead"> Create </h4>
+
+      <div className="fixed-bottom px-5 py-5">
+        <h4 id="lead">Design </h4>
+
+        <h4 id="lead"> Code </h4>
+        <h4 id="lead"> Create </h4>
+      </div>
     </div>
   );
 }
