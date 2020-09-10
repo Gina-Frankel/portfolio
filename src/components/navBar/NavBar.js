d72e891c427d7be97e51b5ca9469461319aa7cc6
Gina Frankel <g.frankel22@gmail.com>
2020-09-10 19:44:49 +0100

    :lipstick: Remove container + padding from NavBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,11 +3,9 @@ import { ContactLinkList, NavBarLinks } from "../index";
 
 export function NavBar() {
   return (
-    <nav className="navbar  sticky-top navbar-expand bg-dark  ">
-      <div className="container py-4">
-        <NavBarLinks />
-        <ContactLinkList />
-      </div>
+    <nav className="navbar  sticky-top navbar-expand bg-dark px-5 ">
+      <NavBarLinks />
+      <ContactLinkList />
     </nav>
   );
 }
