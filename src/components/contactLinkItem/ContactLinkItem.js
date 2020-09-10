b035a46ebafe19b984f3e7571625e46e0b179bdd
Gina Frankel <g.frankel22@gmail.com>
2020-09-10 12:21:08 +0100

    :fire: Remove comments

--- src/components/contactLinkItem/ContactLinkItem.js
+++ src/components/contactLinkItem/ContactLinkItem.js
@@ -6,13 +6,10 @@ export function ContactLinkItem(props) {
     <li className="nav-item px-3">
       <a
         className="nav-link"
-        // className="contactLink  px-2"
         href={props.value.link}
         target="_blank"
         rel="noopener noreferrer"
       >
-        {/* <img src={props.value.icon} alt={props.value.name} /> */}
-
         <IconContext.Provider
           value={{ style: { fontSize: "30px", color: "rgb(27, 182, 187)" } }}
         >
