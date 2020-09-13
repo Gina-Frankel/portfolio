2250d6b690d0ac0c5dab1a24786f1bf9ea4bbe4d
Gina Frankel <g.frankel22@gmail.com>
2020-09-13 11:17:21 +0100

    :lipstick: Switch hero to right and lead to left

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,26 +4,30 @@ import "./Home.css";
 export function Home() {
   return (
     <div className="px-5 container-fluid vertical-stretch">
-      <div className="row justify-content-end ">
-        <div className="">
-          <h1 id="developerName" className="text-right pt-3">
+      <div className="row justify-content-start ">
+        <div className="col-lg-7 col-md-12">
+          <h1 id="developerName" className="text-left pt-3">
             Gina Frankel
           </h1>
         </div>
       </div>
-      <div className="row justify-content-end ">
+      <div className="row justify-content-start ">
         <div className="col-lg-7 col-md-12">
-          <h3 id="lead" className="text-right">
+          <h3 id="lead" className="text-left">
             Junior Full Stack Developer
           </h3>
         </div>
       </div>
-
       <div className="fixed-bottom px-5 py-5">
-        <h4 id="lead">Design </h4>
-
-        <h4 id="lead"> Code </h4>
-        <h4 id="lead"> Create </h4>
+        <div className="row justify-content-end ">
+          <h4 id="lead">Design </h4>
+        </div>
+        <div className="row justify-content-end ">
+          <h4 id="lead">Code </h4>
+        </div>
+        <div className="row justify-content-end ">
+          <h4 id="lead">Create </h4>
+        </div>
       </div>
     </div>
   );
