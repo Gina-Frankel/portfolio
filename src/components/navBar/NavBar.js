26afb780af781d9a4ce00132344af962236c7fa5
Gina Frankel <g.frankel22@gmail.com>
2020-09-13 13:37:50 +0100

    :lipstick: Add background color to navBar
    - Without color when scroll up rest of content can be seen under nav bar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,9 +1,11 @@
 import React from "react";
+
+import "./navBar.css";
 import { ContactLinkList, NavBarLinks } from "../index";
 
 export function NavBar() {
   return (
-    <nav className="navbar  sticky-top navbar-expand px-5 ">
+    <nav className="navbar sticky-top navbar-expand color px-5 ">
       <NavBarLinks />
       <ContactLinkList />
     </nav>
