a8a78e6deb230136e96df1ca13370297123c98d7
Gina Frankel <g.frankel22@gmail.com>
2020-09-13 10:53:34 +0100

    :lipstick: Remove color in navBar

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -3,7 +3,7 @@ import { ContactLinkList, NavBarLinks } from "../index";
 
 export function NavBar() {
   return (
-    <nav className="navbar  sticky-top navbar-expand bg-dark px-5 ">
+    <nav className="navbar  sticky-top navbar-expand px-5 ">
       <NavBarLinks />
       <ContactLinkList />
     </nav>
